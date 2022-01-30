5cae7256ad2f88658754610e033e0059dda1d190
Ryo Aita <792803+aita@users.noreply.github.com>
2022-01-30 09:16:21 +0900

    A Final Render

--- src/main.rs
+++ src/main.rs
@@ -51,55 +51,90 @@ fn rgb_color(pixel_color: &DVec3, samples_per_pixel: u32) -> Rgb<u8> {
     Rgb([r, g, b])
 }
 
+fn random_scene() -> HittableList {
+    let ground_material: Rc<dyn Material> = Rc::new(Lambertian::new(DVec3::new(0.5, 0.5, 0.5)));
+
+    let mut world = HittableList {
+        objects: vec![Box::new(Sphere::new(
+            DVec3::new(0.0, -1000.0, -1.0),
+            1000.0,
+            ground_material,
+        ))],
+    };
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = thread_rng().gen_range(0.0..1.0);
+            let center = DVec3::new(
+                a as f64 + 0.9 * thread_rng().gen_range(0.0..1.0),
+                0.2,
+                b as f64 + 0.9 * thread_rng().gen_range(0.0..1.0),
+            );
+
+            if (center - DVec3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                let sphere_material: Rc<dyn Material> = if choose_mat < 0.8 {
+                    let color = DVec3::new(
+                        thread_rng().gen_range(0.0..1.0),
+                        thread_rng().gen_range(0.0..1.0),
+                        thread_rng().gen_range(0.0..1.0),
+                    );
+                    let albedo = color * color;
+                    Rc::new(Lambertian::new(albedo))
+                } else if choose_mat < 0.95 {
+                    let albedo = DVec3::new(
+                        thread_rng().gen_range(0.5..1.0),
+                        thread_rng().gen_range(0.5..1.0),
+                        thread_rng().gen_range(0.5..1.0),
+                    );
+                    let fuzz = thread_rng().gen_range(0.0..0.5);
+                    Rc::new(Metal::new(albedo, fuzz))
+                } else {
+                    Rc::new(Dielectric::new(1.5))
+                };
+                world
+                    .objects
+                    .push(Box::new(Sphere::new(center, 0.2, sphere_material)));
+            }
+        }
+    }
+
+    world.objects.push(Box::new(Sphere::new(
+        DVec3::new(0.0, 1.0, 0.0),
+        1.0,
+        Rc::new(Dielectric::new(1.5)),
+    )));
+
+    world.objects.push(Box::new(Sphere::new(
+        DVec3::new(-4.0, 1.0, 0.0),
+        1.0,
+        Rc::new(Lambertian::new(DVec3::new(0.4, 0.2, 0.1))),
+    )));
+
+    world.objects.push(Box::new(Sphere::new(
+        DVec3::new(4.0, 1.0, 0.0),
+        1.0,
+        Rc::new(Metal::new(DVec3::new(0.7, 0.6, 0.5), 0.0)),
+    )));
+
+    world
+}
+
 fn main() {
     let mut rng = thread_rng();
 
-    const ASPECT_RATIO: f64 = 16.0 / 9.0;
-    const WIDTH: u32 = 400;
+    const ASPECT_RATIO: f64 = 3.0 / 2.0;
+    const WIDTH: u32 = 1200;
     const HEIGHT: u32 = ((WIDTH as f64) / ASPECT_RATIO) as u32;
-    const SAMPLES_PER_PIXEL: u32 = 100;
+    const SAMPLES_PER_PIXEL: u32 = 500;
     const MAX_DEPTH: u32 = 50;
 
-    let material_ground: Rc<dyn Material> = Rc::new(Lambertian::new(DVec3::new(0.8, 0.8, 0.0)));
-    let material_center: Rc<dyn Material> = Rc::new(Lambertian::new(DVec3::new(0.1, 0.2, 0.5)));
-    let material_left: Rc<dyn Material> = Rc::new(Dielectric::new(1.5));
-    let material_right: Rc<dyn Material> = Rc::new(Metal::new(DVec3::new(0.8, 0.6, 0.2), 0.0));
+    let world = random_scene();
 
-    let world = HittableList {
-        objects: vec![
-            Box::new(Sphere::new(
-                DVec3::new(0.0, -100.5, -1.0),
-                100.0,
-                material_ground.clone(),
-            )),
-            Box::new(Sphere::new(
-                DVec3::new(0.0, 0.0, -1.0),
-                0.5,
-                material_center.clone(),
-            )),
-            Box::new(Sphere::new(
-                DVec3::new(-1.0, 0.0, -1.0),
-                0.5,
-                material_left.clone(),
-            )),
-            Box::new(Sphere::new(
-                DVec3::new(-1.0, 0.0, -1.0),
-                -0.45,
-                material_left.clone(),
-            )),
-            Box::new(Sphere::new(
-                DVec3::new(1.0, 0.0, -1.0),
-                0.5,
-                material_right.clone(),
-            )),
-        ],
-    };
-
-    let lookfrom = DVec3::new(3.0, 3.0, 2.0);
-    let lookat = DVec3::new(0.0, 0.0, -1.0);
+    let lookfrom = DVec3::new(13.0, 3.0, 2.0);
+    let lookat = DVec3::new(0.0, 0.0, 0.0);
     let vup = DVec3::new(0.0, 1.0, 0.0);
-    let dist_to_focus = (lookfrom - lookat).length();
-    let aperture = 2.0;
+    let dist_to_focus = 10.0;
+    let aperture = 0.1;
 
     let camera = Camera::new(
         lookfrom,
